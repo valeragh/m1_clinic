67a5a4afe06758f0ec8e4888ad3794b38127059a
valeragh <valeragh@gmail.com>
2015-09-10 00:27:56 +0300

    add user and activeadmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
+  def access_denied(exception)
+    redirect_to root_path, :alert => exception.message
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  devise_for :users,
+             #controllers: { :omniauth_callbacks => 'users/omniauth_callbacks' },
+             path: '',
+             path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
+  
+  ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
