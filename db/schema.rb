fa7127416edafd247a133d16a01f1f31f3c8a614
valeragh <valeragh@gmail.com>
2015-10-05 17:09:42 +0300

    add new images

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,24 @@ ActiveRecord::Schema.define(version: 20150918073138) do
   add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
 
+  create_table "admin_users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
   create_table "certificates", force: true do |t|
     t.string   "image_url"
     t.integer  "user_id"
@@ -102,7 +120,7 @@ ActiveRecord::Schema.define(version: 20150918073138) do
   create_table "recipes", force: true do |t|
     t.string   "name"
     t.text     "instructions"
-    t.decimal  "price"
+    t.decimal  "price",        precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
