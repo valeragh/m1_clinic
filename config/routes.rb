2946b64fcaeeb1762bcf8d9d77ed3fe03bd60e4f
valeragh <valeragh@gmail.com>
2015-10-31 08:53:41 +0200

    add user icon

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,6 @@ Rails.application.routes.draw do
    match '/laboratory', to: 'static_pages#laboratory', via: 'get'
    match '/administration', to: 'static_pages#administration', via: 'get'
    match '/history', to: 'static_pages#history', via: 'get'
-   match '/partners', to: 'static_pages#partners', via: 'get'
    match '/news', to: 'news#index', via: 'get'
    get '/news/:id', to: 'news#show', as: :show_news
    match '/procedures', to: 'procedures#index', via: 'get'
