2fb0fab801232e2de52741a84462e4c8f0f25be5
valeragh <valeragh@gmail.com>
2015-11-04 10:16:54 +0200

    add registration controller to device

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def errors_for(model, attribute)
+    if model.errors[attribute].present?
+      content_tag :span, :class => 'error_explanation' do
+        model.errors[attribute].join(", ")
+      end
+    end
+  end
 end
