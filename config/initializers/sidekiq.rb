7b11381c2a4f2da4e0c91b6afe040ea06a2ca8d0
valeragh <valeragh@gmail.com>
2015-12-05 22:40:44 +0200

    edit url redis sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'http://redis5.locum.ru:6379' }
+  config.redis = { url: 'redis://redis5.locum.ru:6379' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'http://redis5.locum.ru:6379' }
+  config.redis = { url: 'redis://redis5.locum.ru:6379' }
 end
